bc5f963cba90fee39acb7aaac7b36a9d5c1d5c3e
Lunathor <mrcoopgmz@gmail.com>
2024-05-13 21:46:37 +0300

    Добавлены настройки соединения проекта с yandex почтой для отправки сообщений

--- project/project/settings.py
+++ project/project/settings.py
@@ -152,3 +152,14 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 LOGIN_REDIRECT_URL = "/products"
 
 ACCOUNT_FORMS = {"signup": "accounts.forms.CustomSignupForm"}
+
+# EMAIL_BACKEND SETTINGS
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = "HoneyLunathor@yandex.com"
+EMAIL_HOST_PASSWORD = "rpvdvsuzixrzliol"
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
+DEFAULT_FROM_EMAIL = "HoneyLunathor@yandex.com"
